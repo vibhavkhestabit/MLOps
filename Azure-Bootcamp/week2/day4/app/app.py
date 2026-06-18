90c588333a387dd42f406d6c4eb5d806cebfc7a4
vibhavkhestabit <vibhavk.hestabit@gmail.com>
2026-06-18 20:04:46 +0530

    Day 4 — Full CI/CD Pipeline

--- Azure-Bootcamp/week2/day4/app/app.py
+++ Azure-Bootcamp/week2/day4/app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Azure DevOps CI/CD Pipeline Working!"
+    return "Azure DevOps CI/CD Pipeline Working!, dual build in pipleine working fine"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
